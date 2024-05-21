69653fc716aa2a3b44777575722ae27d02121312
Sergei <95417457+nowelflantier@users.noreply.github.com>
2024-05-21 22:32:09 +0200

    ajout wpa

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,23 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "EVM Checkin App",
+  title: "EVM  App",
   description: "An app for checking in participants at EVM events",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+      <head>
+        {/* <title>{metadata.title}</title> */}
+        {/* <meta name="description" content={metadata.description} /> */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0070f3" />
         <link rel="icon" href="/icons/icon-192x192.png" />
-      </Head>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
